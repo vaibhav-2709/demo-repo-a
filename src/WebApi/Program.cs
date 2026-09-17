19f015430f4e3999b0056b00106afb44b78a5796
Vaibhav Dutt Trivedi <trivedivaibhav0111@gmail.com>
2026-09-17 23:30:23 +0530

    Add basic logging setup in Program.cs

---
+++ src/WebApi/Program.cs
@@ -0,0 +1,14 @@
+using Serilog;
+
+namespace WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+    }
+}
